f20e9ccb42f3aaac66b5973171f642d400dcd79a
Zachary Williams <zwilliams.dev@gmail.com>
2023-06-22 21:34:41 -0600

    initial commit and help message

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Ed
+
+This project is me writing an Ed like editor in Python.
+
+## Scope of Project
+
+This project will imitate the basic features of Ed. It is intended to be
+practice for working with files and user input in python, it is not intended to
+be a 1:1 recreation of Ed.
---
+++ pyed.py
@@ -0,0 +1,10 @@
+def show_help() -> None:
+    print("Usage: pyhelp [options] [file]\n")
+    print("Ed inspired editor written in Python.\n")
+    print("Options:")
+    print("  -h  --help     Prints this help message")
+    print("  -p  --prompt   String to display during interactive prompt")
+
+
+if __name__ == "__main__":
+    show_help()
