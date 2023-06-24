fc3fe2011990eb569106bfc6187b41cf801631a9
Zachary Williams <zwilliams.dev@gmail.com>
2023-06-23 23:59:26 -0600

    major refactor

--- pyed.py
+++ pyed.py
@@ -1,5 +1,11 @@
+from io import TextIOWrapper
 import os.path
 import sys
+
+# Default file name if none provided
+file_name: str = "default.txt"
+file_handle: TextIOWrapper
+file_contents: list[str] = []
 
 
 # TODO: Add a prompt option
@@ -12,32 +18,71 @@ def show_help() -> None:
     sys.exit(0)
 
 
-def edit_file(file) -> None:
-    """Open the file in the editor."""
+def create_open_file(file) -> None:
+    """
+    Create the file if it does not exist or open it.
+    Args:
+        file: The file to open or create
+    """
+    global file_name
+    global file_handle
 
     # Check if the file exists
-    if not os.path.isfile(file):
+    if not os.path.isfile(file_name):
         # Ed will warn that the file does not exist
         # but will allow you to continue anyway
         print(f"{file} no such file or directory.")
         # Create the file in read and write mode
-        file = open(file, "w+")
+        file_handle = open(file, "w+")
     else:
         # Open file in read and append mode
-        file = open(file, "a+")
+        file_handle = open(file, "a+")
 
-    # Open the file in the editor
-    editing: bool = True
-    lines: list = []
-    while editing:
-        command = input("")
-        if command == ".":
-            for line in lines:
-                file.write(line + "\n")
-            file.close()
-            editing = False
+
+def edit_file() -> None:
+    """Take user input and append to gloabl file_contents."""
+    global file_contents
+    while True:
+        line = input()
+        # Start a command
+        if line == ".":
+            command_mode()
+        # Append the line to the files file_contents
         else:
-            lines.append(command)
+            file_contents.append(line + "\n")
+
+
+def write_file() -> None:
+    """Write the file to disk."""
+    global file_handle
+    global file_contents
+
+    # Write the file to disk
+    file_handle.writelines(file_contents)
+    # Close the file
+    file_handle.close()
+
+
+def command_mode() -> None:
+    """Enter command mode."""
+    while True:
+        # TODO: Respect prompt once implemented
+        command = input("")
+        # Quit without saving
+        if command == "q":
+            sys.exit(0)
+        # Write the file
+        elif command == "w":
+            write_file()
+        # Write the file and quit
+        elif command == "wq":
+            write_file()
+            sys.exit(0)
+        # Go back to editing
+        elif command == "a":
+            edit_file()
+        else:
+            print("?")
 
 
 if __name__ == "__main__":
@@ -51,4 +96,5 @@ if __name__ == "__main__":
         show_help()
     # Assume a file was passed
     else:
-        edit_file(args[0])
+        create_open_file(args[0])
+        command_mode()
